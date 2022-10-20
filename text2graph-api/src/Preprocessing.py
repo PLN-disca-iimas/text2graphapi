b4b89598b382f8559b8c197aec21782dafb98bea
gandres-linux <andres.urbano@comunidad.unam.mx>
2022-10-20 08:23:18 -0500

    Add implementation of module preprocessing and stopwords file

--- text2graph-api/src/Preprocessing.py
+++ text2graph-api/src/Preprocessing.py
@@ -1,49 +1,160 @@
+import re  
+import emoji
 import nltk
+import string
+import codecs
+import contractions
+from emot.emo_unicode import UNICODE_EMOJI, UNICODE_EMOJI_ALIAS, EMOTICONS_EMO
+from flashtext import KeywordProcessor  
+
+
+stopwords = []
+for line in codecs.open('stopwords_english.txt', encoding = "utf-8"):
+    # Remove black space if they exist
+    stopwords.append(line.strip())
+stopwords = dict.fromkeys(stopwords, True)   
+
 
 class Preprocessing(object):
+    """Text parser for the preprocessing.
+
+    Params:
+        pos_tagger: Tagger for part of speech.    
+    
+    Examples:
+        >>> text = "I am an 🤖 hehe :-)). Lets try :D another one 😲. It seems 👌"
+        >>> pre = Preprocessing()
+        >>> pre.handle_emoticons(text)
+        I am an robot face hehe Very happy. Lets try Laughing, big grin or laugh with glasses
+         another one astonished. It seems ok hand
+    """
+
     def __init__(self, pos_tagger=nltk.pos_tag):
         self.pos_tagger = pos_tagger
 
-    # remove blank spaces
     def handle_blank_spaces(self, text: str) -> str:
-        pass
+        """Remove blank spaces.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text without blank space.
+        """    
+        return re.sub(r'\s+', ' ', text).strip()
 
-    # replace/remove special char
     def handle_non_ascii(self, text: str) -> str:
-        pass
-
-    # transform emoji to text
+        """Remove special characters.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text without non_asccii characters.        
+        """
+        regex_non_asccii = f'[^{string.ascii_letters}]'
+        return re.sub(regex_non_asccii, " ", text)
+    
+    
     def handle_emoticons(self, text: str) -> str:
-        pass
-    
-    # remove html tags
+        """Transform emoji to text.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text with emoji text.
+        """
+        ## Join and clen emojis and emoticons
+        all_emoji_emoticons = {**EMOTICONS_EMO,**UNICODE_EMOJI_ALIAS}
+        all_emoji_emoticons = {k:v.replace(":","").replace("_"," ").strip() 
+                                for k,v in all_emoji_emoticons.items()}
+        
+        # Add emojis for remplace
+        kp_all_emoji_emoticons = KeywordProcessor()
+        for k,v in all_emoji_emoticons.items():
+            kp_all_emoji_emoticons.add_keyword(k, v)
+
+        return kp_all_emoji_emoticons.replace_keywords(text)
+        
     def handle_html_tags(self, text: str) -> str:
-        pass
+        """Remove any html tags.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text without tags.
+        """
+        html_pattern = re.compile('<.*?>')
+        return html_pattern.sub(r'', text)  
 
-    # remove stop words
     def handle_stop_words(self, text: str) -> str:
-        pass
+        """Remove stop words
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text without stopwords.        
+        """
+        tokens = self.word_tokenize(text)
+        # Remove las stopwords
+        without_stopwords = [word for word in tokens if not stopwords.get(word.lower().strip(), False)]
+        return " ".join(without_stopwords)
     
-    # expand contractions
     def handle_contractions(self, text: str) -> str:
-        pass
-    
-    # handle negations
+        """Expand contractions.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text without contractions.   
+        """
+        expanded_words = [contractions.fix(word) for word in text.split(" ")]
+        return " ".join(expanded_words)  
+        
     def handle_negations(self, text: str) -> str:
-        pass
+        """Handle negations.        
 
-    # tranform text to lowercase
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text without negations.   
+        """
+        return self.handle_contractions(text)
+
     def to_lowercase(self, text: str) -> str:
-        pass
+        """Tranform text to lowercase.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text in lowercase.   
+        """
+        return text.lower()
 
-    # sentence tokenizer
     def sent_tokenize(self, text: str) -> list:
-        pass
+        """Tokenize by sentece.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text tokenize by sentences.  
+        """
+        return nltk.sent_tokenize(text)
 
-    # work tokenizer
     def word_tokenize(self, text: str) -> list:
-        pass
+        """Tokenize by word.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text tokenize by word.  
+        """
+        return nltk.word_tokenize(text)
 
-    # part of speech tags
     def pos_tagger(self, text: list) -> list:
-        pass
+        """Tagging part of speech.
+        
+        Params:
+            text (str): Text for preprocesesing.
+        Return:
+            str: Text tagged.         
+        """
+        return self.pos_tagger(text)
